06a87979b0a2e431e4cf1e031646b6bca624df28
marytulip <marytulip127@gmail.com>
2024-08-16 08:36:55 -0300

    aula0908

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
@@ -9,9 +9,18 @@ def index():
 @app.route('/aula/<nome>')
 @app.route('/aula/<nome>/<curso>')
 @app.route('/aula/<nome>/<curso>/<int:ano>')
-def aula(nome = 'Jão', curso = 'Informática',ano = 1):
+def aula(nome = 'Mary', curso = 'Informática',ano = 1):
     dados = {'nome':nome,'curso':curso, 'ano':ano}
     return render_template('aula.html',dados_curso = dados)
 
+
+@app.route('/form')
+def form():
+    return render_template('form.html')
+
+@app.route('/dados', methods=['POST'])
+def dados():
+    dados = request.form
+    return render_template('dados.html', dados=dados)
 if __name__ == '__main__':
     app.run()
